f15d7ab3f2b6aabebcf11f985da5261cab3a16b3
odinokun <odinokun@gmail.com>
2024-11-12 19:43:12 -0500

    Fetch todos from API and display them in a list; update App component title

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 function App() {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then(response => response.json())
-      .then(json => console.log(json));
+      //.then(json => console.log(json));
+      .then(json => setTodos(json));
   }, []);
 
   return (
     <div className='App'>
-      <h1>APP</h1>
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo: any) => (
+          <li key={todo.id}>
+            <div>id = {todo.id}</div>
+            <div> title = {todo.title}</div>
+            <div> completed = {todo.completed ? 'true' : 'false'}</div>
+            <br />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
