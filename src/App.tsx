7490db4c6214c895ce470fbb688ad14c8e179adb
odinokun <odinokun@gmail.com>
2024-11-12 19:28:35 -0500

    Fetch and display todos from API; update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import React, {useState} from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
-
-
-
 function App() {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => console.log(json));
+  }, []);
 
-
-
-    return (
-        <div className="App">
-
-        </div>
-    );
+  return (
+    <div className='App'>
+      <h1>APP</h1>
+    </div>
+  );
 }
 
 export default App;
-
