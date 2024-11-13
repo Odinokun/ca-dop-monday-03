e6be1bb0c3953e41746a7934e9028fdaa33a7e5a
odinokun <odinokun@gmail.com>
2024-11-12 21:38:58 -0500

    Add task management features with input handling and button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { Button } from './components/Button';
 
 type TodoType = {
   userId: number;
@@ -10,16 +11,48 @@ type TodoType = {
 
 function App() {
   const [todos, setTodos] = useState<TodoType[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
 
-  useEffect(() => {
+  const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then(response => response.json())
-      //.then(json => console.log(json));
       .then(json => setTodos(json));
+  };
+
+  useEffect(() => {
+    fetchTodos();
   }, []);
 
+  const showTodosHandler = () => fetchTodos();
+  const hideTodosHandler = () => setTodos([]);
+
+  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setInputValue(e.target.value);
+
+  const addTaskHandler = () => {
+    const newTask = {
+      userId: Math.random(),
+      id: Math.random(),
+      title: inputValue,
+      completed: false,
+    };
+    setTodos([newTask, ...todos]);
+    setInputValue('');
+  };
+
   return (
-    <div className='App'>
+    <div>
+      <div>
+        <Button name='Show me todos' onClick={showTodosHandler} />
+        <Button name='Hide todos' onClick={hideTodosHandler} />
+      </div>
+      <br />
+
+      <div>
+        <input type='text' value={inputValue} onChange={inputChangeHandler} />
+        <Button name='Add task' onClick={addTaskHandler} />
+      </div>
+
       <ul>
         {todos.map((todo: TodoType) => (
           <li key={todo.id}>
